fe64cae13f455cf086dbfcd45bb819969af48c19
Dan Horrigan <dan@dhorrigan.com>
2011-04-18 19:29:51 -0400

    Fixing the routes.

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,5 +1,6 @@
 <?php
 return array(
-	'_root_'  => 'scraps',  // The default route
+	'_root_'  => 'scraps/index',  // The default route
 	'_404_'   => 'scraps/404',    // The main 404 route
+	'(:any)'   => 'scraps/$1',
 );
